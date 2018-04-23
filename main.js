05139e716fee92353b593336a840d899e7ced580
Alejandro <38469669+afox1789@users.noreply.github.com>
2018-04-23 14:13:36 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+var alert;
+var document;
+//var miTitulo = document.querySelector("h1");
+//miTitulo.innerHTML = "Hello world!";
+
+/*var helado = "fresa";
+if (helado === "chocolate") {
+    alert("Si, amo el helado de chocolate!");
+} else {
+    alert("Awww, pero mi favorito es el de chocolate...!");
+}*/
+/*var a = 10;
+var b = 12;
+var suma = a + b;
+if (suma >= 100) {
+    alert("Eres correcto!");
+} else {
+    alert("No sabes nada jaja!");
+}*/
+/*document.querySelector("html").onclick = function () {
+    "use strict";
+    alert("Ouch! Deja de pincharme!!");
+}*/
+/*document.querySelector("h3").onclick = function () {
+    "use strict";
+    alert("AJA!! Me descubriste!!");
+}*/
+/*var miImage = document.querySelector("img");
+miImage.onclick = function () {
+    "use strict";
+    var miSrc = miImage.getAttribute("src");
+    if (miSrc === "images/2.gif") {
+        miImage.setAttribute("src", "images/5.jpg");
+    } else {
+        miImage.setAttribute("src", "images/2.gif");
+    }
+}*/
+var prompt
+var miBoton = document.querySelector('button');
+var miTitulo = document.querySelector('h1');
+function estableceNombreUsuario() {
+    "use strict";
+var miNombre = prompt("Por favor, ingresa tu nombre.");
+    alert(miNombre);
+}
+    
